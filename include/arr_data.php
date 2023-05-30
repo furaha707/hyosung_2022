defe20da6f97d0cb97e6d3c946d73d8d6b6f1bde
furaha707 <zxcvyein950823@gmail.com>
2023-05-30 13:50:28 +0900

    Initial commit

---
+++ include/arr_data.php
@@ -0,0 +1,98 @@
+<?
+
+$site_title = "(주)효성티앤에스"; // 사이트명
+
+$P_lang = "ko"; // html language
+
+$P_company_name = ""; // 회사명
+
+$P_addr1 = ""; // 주소1
+$P_addr2 = ""; // 주소2
+
+$P_tel1 = "";
+$P_tel2 = "";
+
+$P_fax1 = "";
+$P_fax2 = "";
+
+$P_email1 = "";
+$P_email2 = "";
+
+$P_business_no = ""; // 사업자등록번호
+
+$P_sales_no = ""; // 통신판매업신고번호
+
+// 네비게이션 메뉴명
+$nav_1 = "서비스 소개";
+$nav_1_1 = "POS";
+$nav_1_2 = "키오스크";
+$nav_1_3 = "셀프체크아웃";
+$nav_1_4 = "AI냉장고";
+
+$nav_2 = "매장별 서비스";
+$nav_2_1 = "일반식당";
+$nav_2_2 = "배달전문식당";
+$nav_2_3 = "카페";
+$nav_2_4 = "무인매장";
+$nav_2_5 = "프랜차이즈";
+$nav_2_6 = "마트/편의점";
+
+$nav_3 = "무료로 시작하기";
+$nav_3_1 = "사용신청";
+$nav_3_2 = "사용방법";
+
+$nav_4 = "장비 구매하기";
+
+$nav_5 = "사장님 이야기";
+$nav_5_1 = "사장님 이야기";
+$nav_5_2 = "자주하는 질문";
+$nav_5_3 = "문의게시판";
+$nav_5_4 = "개선요청 게시판";
+$nav_5_5 = "다운로드 게시판";
+
+$nav_policy = "홈페이지 정책";
+$nav_policy_1 = "개인정보처리방침";
+$nav_policy_2 = "이용약관";
+$nav_policy_3 = "이메일무단수집거부";
+
+// 네비게이션 하이퍼링크
+$nav_1_href = "../service/pos.html";
+$nav_1_1_href = "../service/pos.html";
+$nav_1_2_href = "../service/kiosk.html";
+$nav_1_3_href = "../service/self.html";
+$nav_1_4_href = "../service/ai.html";
+
+$nav_2_href = "../store/store.html";
+$nav_2_1_href = "../store/store.html";
+$nav_2_2_href = "../store/delivery.html";
+$nav_2_3_href = "../store/cafe.html";
+$nav_2_4_href = "../store/cashierless.html";
+$nav_2_5_href = "../store/franchise.html";
+$nav_2_6_href = "../store/mart.html";
+
+$nav_3_href = "../application/write.html";
+$nav_3_1_href = "../application/write.html";
+$nav_3_2_href = "../application/manual.html";
+
+$nav_4_href = "#";
+
+$nav_5_href = "../story/story.html";
+$nav_5_1_href = "../story/story.html";
+$nav_5_2_href = "../story/qna.html";
+$nav_5_3_href = "../story/inquiry-list.html";
+$nav_5_4_href = "../story/request-list.html";
+$nav_5_5_href = "../story/download-list.html";
+
+$nav_policy_href = "../policy/privacy.html";
+$nav_policy_1_href = "../policy/privacy.html";
+$nav_policy_2_href = "../policy/term.html";
+$nav_policy_3_href = "../policy/email-security.html";
+
+// 서브비주얼
+$sv_1_name = "";
+$sv_2_name = "";
+$sv_3_name = "";
+$sv_4_name = "";
+$sv_5_name = "";
+
+?>
